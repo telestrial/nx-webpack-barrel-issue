a43cd87460dda21a7847c14f14e997fe260fc8cf
John <john@shineweddinginvitations.com>
2023-03-24 11:06:31 -0400

    add some creature conforts for nagivation through pages

--- apps/woggle/pages/orange-blue-test.tsx
+++ apps/woggle/pages/orange-blue-test.tsx
@@ -1,11 +1,15 @@
-import { BlueBox, OrangeBox } from '@woggle/ui';
-// import { OrangeBox } from '@woggle/ui/orange-box';
-// import { BlueBox } from '@woggle/ui/blue-box';
+// import { BlueBox, OrangeBox } from '@woggle/ui';
+import { OrangeBox } from '@woggle/ui/orange-box';
+import { BlueBox } from '@woggle/ui/blue-box';
+
+// Don't flip this import. Just what's above.
+import Link from 'next/link';
 
 export default function page2() {
   return (
     <div>
-      Blue Box Tree Shake Test
+      <Link href="/">Back</Link>
+      <h3>Blue Box Tree Shake Test</h3>
       <p>
         We would expect to see both orange and blue box code here, but not
         skyblue box code.
--- apps/woggle/pages/orange-test.tsx
+++ apps/woggle/pages/orange-test.tsx
@@ -1,10 +1,14 @@
-import { OrangeBox } from '@woggle/ui';
-// import { OrangeBox } from '@woggle/ui/orange-box';
+// import { OrangeBox } from '@woggle/ui';
+import { OrangeBox } from '@woggle/ui/orange-box';
+
+// Don't flip this import. Just what's above.
+import Link from 'next/link';
 
 export default function Page1() {
   return (
     <div>
-      Orange Box Tree Shake Test
+      <Link href="/">Back</Link>
+      <h3>Orange Box Tree Shake Test</h3>
       <p>We would expect to only see orange box code here</p>
       <OrangeBox />
     </div>
